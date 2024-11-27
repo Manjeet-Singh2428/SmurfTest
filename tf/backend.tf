0f1dbb1f1c5036f7a2863a08ab039450b54a9139
Manjeet-Singh2428 <108221122+Manjeet-Singh2428@users.noreply.github.com>
2024-11-27 23:50:13 +0530

    bucket

--- tf/backend.tf
+++ tf/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "test-tf22"
+    bucket         = "test-tf221"
     key            = "s3/terraform.tfstate"
     region         = "us-west-2"
   }
