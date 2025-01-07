67f92aed77dc302ebe96f5d7828545ebbd5f5c6d
Manjeet-Singh2428 <108221122+Manjeet-Singh2428@users.noreply.github.com>
2025-01-07 22:08:29 +0530

    test provision 2

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "test-tf221"
+    bucket         = "test-tf211"
     key            = "s3/terraform.tfstate"
     region         = "us-west-2"
   }
